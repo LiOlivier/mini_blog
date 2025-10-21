1f79ba524b178ef57aed5b985f0e7d9c76896377
LiOlivier <liolivier98@gmail.com>
2025-10-21 15:28:37 +0200

    Add la template home.html, le css de l'app

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from .models import Post 
 
 def home(request): #request = instance httpRequest
-    Posts = Post.objects.all().order_by('-created_at') # récupération + tri posts décroissant
-    return render(request, 'blog/home.html', {'Posts': Posts}) # post -> template -> context 
+    posts = Post.objects.all().order_by('-created_at') # récupération + tri posts décroissant
+    return render(request, 'blog/home.html', {'posts': posts}) # post -> template -> context 
+
