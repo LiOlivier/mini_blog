66028aea4a003168b4ac59940828f707602c1698
LiOlivier <liolivier98@gmail.com>
2025-10-21 12:51:00 +0200

    Modification de la vue views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post 
 
-# Create your views here.
+def home(request): #request = instance httpRequest
+    Posts = Post.objects.all().order_by('-created_at') # récupération + tri posts décroissant
+    return render(request, 'blog/home.html', {'Posts': Posts}) # post -> template -> context 
