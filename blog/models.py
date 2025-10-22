ba9583b3a4da48a9ab3376a8b791721283d287de
LiOlivier <liolivier98@gmail.com>
2025-10-22 15:30:01 +0200

    Add author of a post

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200) #Titre du post
     content = models.TextField() #Contenue du post
     created_at = models.DateTimeField(default=timezone.now) #Quand le post à été publier 
+    author = models.ForeignKey( 'auth.User', on_delete=models.CASCADE) # auteur du post
 
     def __str__(self):
         return self.title 
