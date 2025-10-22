2e918b066c788449c4990ee1fdfb10dc0643e069
LiOlivier <liolivier98@gmail.com>
2025-10-22 15:57:57 +0200

    change

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=200) #Titre du post
     content = models.TextField() #Contenue du post
     created_at = models.DateTimeField(default=timezone.now) #Quand le post à été publier 
-    author = models.ForeignKey( 'auth.User', on_delete=models.CASCADE) # auteur du post
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title 
